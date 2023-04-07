6ddea5664fd17925e1d6c45a853f0f57b71198bb
ping-techbasecamp <ping@techbase.camp>
2023-04-08 01:20:01 +0700

    feat:add restaurant card and model

---
+++ lib/widgets/restuarant_card.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:jenosize/model/restuarant_model.dart';
+
+class RestuarantCard extends StatelessWidget {
+  const RestuarantCard({Key? key, required this.restaurant}) : super(key: key);
+
+  final Restaurant restaurant;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: Image.network(
+          restaurant.imageProfile,
+          fit: BoxFit.cover,
+        ),
+      ),
+      title: Text(
+        restaurant.name,
+      ),
+      subtitle: Text(
+        restaurant.description,
+        maxLines: 3,
+      ),
+      isThreeLine: true,
+    );
+  }
+}
